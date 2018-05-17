9cbf8b85e70851c8d0f27c11a00e0a6358ca027d
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-05-17 09:13:47 -0300

    [Validação de features] - Geolocation

--- src/components/Geolocation.vue
+++ src/components/Geolocation.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1 class="text-center">{{ msg }}</h1>
+  <div class="text-center">
+    <h1>{{ msg }}</h1>
+    <button class="btn btn-success" @click="getGPSPosition">Obter localização</button>
+    <div class="alert alert-info" style="margin-top: 10px">
+      <p><span class="font-weight-bold">GPS</span>: {{gps.active}}</p>
+      <p><span class="font-weight-bold">Latitude</span>: {{gps.lat}}</p>
+      <p><span class="font-weight-bold">Longitude</span>: {{gps.lon}}</p>
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,35 @@ export default {
   name: 'Geolocation',
   data () {
     return {
-      msg: 'Geolocation'
+      msg: 'Geolocation',
+      gps: {
+        active: false,
+        lon: '...',
+        lat: '...'
+      }
+    }
+  },
+  mounted () {
+    if (navigator.geolocation) {
+      // alert('[Geolocation] - Suportado')
+    } else {
+      alert('[Geolocation] - Não suportado')
+    }
+  },
+  methods: {
+    getGPSPosition: () => {
+      if (!navigator.geolocation) {
+        return
+      }
+      navigator.geolocation.getCurrentPosition((position) => {
+        const {lat, long} = position.coords
+        console.log(lat, position.long)
+        this.gps.active = true
+        this.gps.lat = lat
+        this.gps.long = long
+      })
+    },
+    watchGPSPostion: () => {
     }
   }
 }
